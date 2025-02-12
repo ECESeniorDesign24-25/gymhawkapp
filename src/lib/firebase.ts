c525b4a61ed8057943c0959965f91aa77fb36cd0
Joseph <98860676+jkrue242@users.noreply.github.com>
2025-02-11 18:30:20 -0600

    Env variable support (#2)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBhkNpHxs8uRQeOBYqftm2yzmtFSUyacmw",
-  authDomain: "gymhawk-2ed7f.firebaseapp.com",
-  projectId: "gymhawk-2ed7f",
-  storageBucket: "gymhawk-2ed7f.firebasestorage.app",
-  messagingSenderId: "374604200780",
-  appId: "1:374604200780:web:7ca59e75450e0a4370ede9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
