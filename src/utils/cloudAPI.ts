25ba77378bb2504c7223ba4978bf7800e798cb3b
Joseph <98860676+jkrue242@users.noreply.github.com>
2025-03-03 20:07:56 -0600

    Cron job for db population (#11)

--- src/utils/cloudAPI.ts
+++ src/utils/cloudAPI.ts
@@ -1,6 +1,18 @@
+import { getDocs, collection, query } from "firebase/firestore"; 
+import { db } from "@/lib/firebase"
+
 export async function fetchDeviceState(machine: string, signal?: AbortSignal) {
   try {
-    const response = await fetch(`https://gymhawk-2ed7f.web.app/api/getDeviceState?machine=${machine}`, { signal });
+    // always default to loading state
+    const machines = collection(db, "machines");
+    const querySnapshot = await getDocs(machines);
+    const thing_id = querySnapshot.docs.find((doc) => doc.id === machine)?.data().thingId;
+
+    if (!thing_id) {
+      return "loading";
+    }
+
+    const response = await fetch(`https://gymhawk-2ed7f.web.app/api/getDeviceState?thing_id=${thing_id}`, { signal });
 
     if (!response.ok) {
       return "loading";
@@ -10,11 +22,11 @@ export async function fetchDeviceState(machine: string, signal?: AbortSignal) {
     const buffer = await response.arrayBuffer();
     const utf8Text = new TextDecoder('utf-8').decode(buffer);
     const data = JSON.parse(utf8Text);
-
     return data.state;
   } catch (err: any) {
     if (err.name === 'AbortError') {
-      console.log('Fetch aborted for machine:', machine);
+      console.log('Fetch aborted for thing:', machine);
+      return "loading";
     } else {
       console.error('Failed to fetch device state:', err);
       return "loading";
