b5749b7f5c12b06b4c22d51b183a5d2f9fbfddad
jkrue242 <josephkrueger242@gmail.com>
2025-04-05 23:11:55 -0500

    simulated data

--- functions/main.py
+++ functions/main.py
@@ -1,5 +1,3 @@
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
 from firebase_functions import https_fn, scheduler_fn
 from firebase_admin import initialize_app, firestore
 import iot_api_client as iot
@@ -7,186 +5,21 @@ from iot_api_client.rest import ApiException
 from iot_api_client.configuration import Configuration
 from iot_api_client.api import PropertiesV2Api
 from iot_api_client.models import *
-from firebase_functions.params import StringParam
-import os
 import json
-from google.cloud.sql.connector import Connector, IPTypes
-from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
 from datetime import datetime, timezone, time
 import time as t
-
-load_dotenv(".env.gymhawk-2ed7f")
+from utils import *
+from consts import *
 
 # set up firebase app
 initialize_app()
 db = firestore.client()
-
-# from .env.gymhawk-2ed7f file
-ARDUINO_CLIENT_ID = StringParam("ARDUINO_CLIENT_ID").value
-ARDUINO_CLIENT_SECRET = StringParam("ARDUINO_CLIENT_SECRET").value
-DB_NAME = os.environ.get("DB_NAME") or StringParam("DB_NAME").value
-DB_USER = os.environ.get("DB_USER") or StringParam("DB_USER").value
-DB_PASS = os.environ.get("DB_PASS") or StringParam("DB_PASS").value
-DB_INSTANCE_NAME = (
-    os.environ.get("DB_INSTANCE_NAME") or StringParam("DB_INSTANCE_NAME").value
-)
-
-connection_pool = None
 
 
 class ManualRequest:
     def __init__(self, args):
         self.method = None
         self.args = args
-
-
-# =============================================================================
-# Util functions
-# =============================================================================
-
-
-def init_db_connection():
-    """
-    Initialize the database connection
-    """
-    global connection_pool
-
-    # cloud sql python connector
-    connector = Connector(refresh_strategy="LAZY")
-
-    def _connect():
-        """
-        Connect to the database
-        """
-        conn = connector.connect(
-            DB_INSTANCE_NAME,
-            "pg8000",
-            user=DB_USER,
-            password=DB_PASS,
-            db=DB_NAME,
-            ip_type=IPTypes.PUBLIC,
-        )
-        return conn
-
-    if not connection_pool:
-        connection_pool = create_engine(
-            "postgresql+pg8000://",
-            creator=_connect,
-            pool_size=2,
-            max_overflow=2,
-            pool_timeout=30,
-            pool_recycle=1800,
-        )
-    return connection_pool
-
-
-def test_connection():
-    try:
-        engine = init_db_connection()
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT 1"))
-            print("Database connection successful:", result.scalar())
-        return True
-    except Exception as e:
-        print("Connection failed:", str(e))
-        return False
-
-
-def write_state_to_db(thing_id: str, state: str, current: int, n_on: int, n_off: int, timestamp: str) -> None:
-    try:
-        engine = init_db_connection()
-        with engine.connect() as conn:
-            conn.execute(
-                text(
-                    "INSERT INTO machine_states (thing_id, state, current, timestamp, n_on, n_off) "
-                    "VALUES (:thing_id, :state, :current, :timestamp, :n_on, :n_off)"
-                ),
-                {
-                    "thing_id": thing_id,
-                    "state": state,
-                    "current": current,
-                    "timestamp": timestamp,
-                    "n_on": n_on,
-                    "n_off": n_off,
-                },
-            )
-            conn.commit()
-    except Exception as e:
-        print(f"Error writing to db: {e}")
-        raise
-
-
-def fetch_state_from_db(machine: str, startTime: str) -> list:
-    try:
-        engine = init_db_connection()
-        with engine.connect() as conn:
-            result = conn.execute(
-                text(
-                    "SELECT state, timestamp FROM machine_states WHERE thing_id = :machine AND timestamp >= :startTime ORDER BY timestamp"
-                ),
-                {"machine": machine, "startTime": startTime},
-            )
-
-            # serializeable format
-            return [
-                {"state": row[0], "timestamp": row[1].isoformat()} for row in result
-            ]
-    except Exception as e:
-        print(f"Error fetching from db: {e}")
-        raise
-
-
-# Adapted from: https://github.com/arduino/iot-client-py/blob/master/example/main.py
-def get_token():
-    """
-    Get the token for the arduino cloud API
-    """
-    oauth_client = BackendApplicationClient(client_id=ARDUINO_CLIENT_ID)
-    token_url = "https://api2.arduino.cc/iot/v1/clients/token"
-    oauth = OAuth2Session(client=oauth_client)
-    token = oauth.fetch_token(
-        token_url=token_url,
-        client_id=ARDUINO_CLIENT_ID,
-        client_secret=ARDUINO_CLIENT_SECRET,
-        include_client_id=True,
-        audience="https://api2.arduino.cc/iot",
-    )
-    return token.get("access_token")
-
-
-def get_thing_id(machine):
-    """
-    Get the thing id for a machine from firestore
-    """
-    doc = db.collection("machines").document(machine).get()
-    if doc.exists:
-        data = doc.to_dict()
-        return str(data.get("thingId"))
-    print(f"{machine} does not have a corresponding doc in firestore")
-    return None
-
-
-
-def addTimeStepUtil(thing_id: str, timestamp: str) -> tuple[str, float]:
-    req = ManualRequest(args={"thing_id": thing_id})
-    device_state = getDeviceState(req)
-    if device_state and device_state.data:
-        state_data = json.loads(device_state.data.decode("utf-8"))
-        state = state_data.get("state")
-        current = state_data.get("current")
-        return state, current 
-
-    else:
-        print("Device state is None")
-
-
-def is_time_between(begin_time, end_time, current_time):
-    current_time = current_time.time()
-    if begin_time < end_time:
-        return current_time >= begin_time and current_time <= end_time
-    else:
-        return current_time >= begin_time or current_time <= end_time
 
 # =============================================================================
 # Cloud Functions
